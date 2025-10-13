9702553eaf97093a6e43170df54944df2499a725
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-10-13 17:59:51 +0500

    Allowing image srcs from remote urls and svg formats in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Letting Next.js know that we can use the placehold.co url as image source
+
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
