1c6e828f30e5af9cd808d7da15fae7e3f4f3351d
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-11-01 18:54:56 +0500

    Created Sanity document types of author and startup, installed and added sanity-plugin-markdown

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,50 @@
+import { Lightbulb, UserIcon } from "lucide-react";
+import { defineField, defineType } from "sanity";
+
+export const startup = defineType({
+  name: "startup",
+  title: "Startup",
+  type: "document",
+  icon: Lightbulb,
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title", // Slug will be auto-generated based on title of startup
+      },
+    }),
+    defineField({
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
+    }),
+    defineField({
+      name: "views",
+      type: "number",
+    }),
+    defineField({
+      name: "description",
+      type: "text",
+    }),
+    defineField({
+      name: "category",
+      type: "string",
+      validation: (Rule) =>
+        Rule.min(1).max(20).required().error("Please enter a category"),
+    }),
+    defineField({
+      name: "image",
+      type: "url",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "pitch",
+      type: "markdown", // Custom field type which uses the markdown plugin by sanity
+    }),
+  ],
+});
