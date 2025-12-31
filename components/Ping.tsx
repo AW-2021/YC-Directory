a1dddb87b55d4c405c9acbd048dd8829fe7526f3
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-12-31 14:52:16 +0500

    Set up a Sanity write client

--- components/Ping.tsx
+++ components/Ping.tsx
@@ -4,9 +4,9 @@ const Ping = () => {
   return (
     <div className="relative">
       <div className="absolute -left-4 top-1">
-        <span className="flex size-[11px]">
+        <span className="flex size-2.75">
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75"></span>
-          <span className="relative inline-flex size-[11px] rounded-full bg-primary"></span>
+          <span className="relative inline-flex size-2.75 rounded-full bg-primary"></span>
         </span>
       </div>
     </div>
--- components/View.tsx
+++ components/View.tsx
@@ -1,6 +1,18 @@
 import Ping from "@/components/Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
-const View = ({ id }: { id: string }) => {
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  await writeClient
+    .patch(id) // patch used for updates
+    .set({ views: totalViews + 1 })
+    .commit();
+
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
@@ -8,7 +20,7 @@ const View = ({ id }: { id: string }) => {
       </div>
 
       <p className="view-text">
-        <span className="font-black">100 views</span>
+        <span className="font-black">Views: {totalViews}</span>
       </p>
     </div>
   );
